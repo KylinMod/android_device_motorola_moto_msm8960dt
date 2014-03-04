7ad59227fda024566f6610801d8ec6b2d10848bf
zerojim kylin <kylinmod@gmail.com>
2014-03-04 23:12:40 +0800

    Kylin Modify

---
+++ kylin.mk
@@ -0,0 +1,14 @@
+# Inherit some common KM stuff.
+$(call inherit-product, vendor/kylin/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/kylin/config/nfc_enhanced.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Release name
+PRODUCT_NAME := km_moto_msm8960dt
+
+$(call inherit-product, device/motorola/moto_msm8960dt/full_moto_msm8960dt.mk)
